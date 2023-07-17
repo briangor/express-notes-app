c871b9a44651ce532e6d4975732c6e9cebea1e12
briangor <briangor13@gmail.com>
2023-07-17 17:34:23 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express'
+
+// import db function 
+import { getNote, getNotes, createNote } from './database'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/notes', async (req, res) => {
+    //res.send("this should be the notes")
+    const notes = await getNotes()
+    res.send(notes)
+})
+
+app.get('/notes/:id', async (req, res) => {
+    const id = req.params.id
+    const note = await getNote(id)
+    res.send(note)
+})
+
+app.post('/notes', async (req, res) => {
+    const { title, contents } = req.body // express grabs data from http
+    const note = await createNote(title, contents)
+    res.status(201).send(note)
+})
+
+app.use((err, req, res, next) => {
+    console.log(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080')
+})
---
+++ database.js
@@ -0,0 +1,50 @@
+import mysql from 'mysql2'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise()
+
+export async function getNotes() {
+    const [rows] = await pool.query("SELECT * FROM notes")
+    return rows
+
+}
+
+export async function getNote(id) {
+    const [rows] = await pool.query(`
+    SELECT * 
+    FROM notes
+    WHERE id = ?
+    `, [id])
+    return rows
+} // test with where id = ${id} and access with no authorization/
+
+export async function createNote(title, contents) {
+    const [result] =  await pool.query(`
+    INSERT INTO notes (title, contents)
+    VALUES (?, ?)
+    `, [title, contents])
+    //return result.insertId
+    /* return {
+        id: result.insertId,
+        title,
+        content
+    } */
+    const id = result.insertId
+    return getNote(id)
+}
+
+// const notes = await getNotes()
+// console.log(notes)
+
+// const note = await getNote(1)
+// console.log(note)
+
+// const result = await createNote('test', 'test')
+// console.log(result)
