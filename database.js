f8e112ffd2555842d285400e079ec631529ff0d7
briangor <briangor13@gmail.com>
2024-02-28 21:41:30 +0300

    add comments for db implementations

--- database.js
+++ database.js
@@ -3,12 +3,61 @@ import mysql from 'mysql2'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const pool = mysql.createPool({
+const dbConfig = {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).promise()
+    database: process.env.MYSQL_DATABASE,
+    connectionLimit: 10, 
+}
+
+const pool = mysql.createPool(dbConfig).promise()
+
+// createConnection
+/* const connection = mysql.createConnection(dbConfig)
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    if (err) throw err
+
+    console.log('The solution is: ', rows[0].solution)
+})
+
+connection.query('SELECT 1 + 2 AS solution', (err, rows, fields) => {
+    if (err) throw err
+
+    console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end() */
+
+// createPool
+// Simulating multiple queries simultaneously
+/*
+for (let i = 0; i < 5; i++) {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting connection from pool:', err);
+      return;
+    }
+
+    console.log(`Connected to database using createPool - Connection ${i + 1}`);
+
+    // Simulating a query
+    connection.query('SELECT * FROM notes', (queryErr, results) => {
+      if (queryErr) {
+        console.error('Error executing query:', queryErr);
+      } else {
+        console.log(`Query results (${i + 1}):`, results);
+      }
+
+      // Release the connection back to the pool when done
+      connection.release();
+    });
+  });
+}
+*/
+
 
 // Get all notes
 export async function getNotes() {
